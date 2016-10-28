5e65fa1d11738c1e00361d0712ca95644320cc53
vlad19182 <dima.vlad@facebook.com>
2016-10-28 12:01:06 +0000

    vibrate for 1 sec

--- www/index.js
+++ www/index.js
@@ -3,4 +3,5 @@ document.getElementById('button').onclick = function () {
   counterValue = parseInt(document.getElementById('counter').innerHTML);
   counterValue = counterValue + 1;
   document.getElementById('counter').innerHTML = counterValue;
+  navigator.notification.vibrate(1000);
 }
